7469936c6e2c02477352fd1f47cab374f86b7c9b
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2015-09-25 00:19:01 -0300

    Prueba básica.

---
+++ test/src/GameTest.cpp
@@ -0,0 +1,11 @@
+
+#include "gtest/gtest.h"
+#include "Game.h"
+
+/* hack/truco para poder correr las pruebas sin copiar los fuentes */
+#include "Game.cpp"
+
+TEST(GameTest, crearSinParametros) {
+
+    Game primerGame;
+}
